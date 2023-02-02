2dbee76cd96a0d6f8d62b00e0f5b23d818a03b34
Marco-Burattoni <marco.burattoni@studio.unibo.it>
2023-02-02 08:50:40 +0100

    viewmenu

--- uwait/src/view/InterfacciaOrdine/ViewMenu.jsx
+++ uwait/src/view/InterfacciaOrdine/ViewMenu.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
 
 function ViewMenu() {
+  const menu = [
+    {
+      nome: 'Pizza Margherita',
+      descrizione: 'Pomodoro, mozzarella, basilico',
+      prezzo: 8,
+      allergeni: ['glutine']
+    },
+    {
+      nome: 'Pasta al pomodoro',
+      descrizione: 'Pasta con salsa di pomodoro fresco',
+      prezzo: 7,
+      allergeni: []
+    },
+    {
+      nome: 'Insalata caprese',
+      descrizione: 'Mozzarella, pomodori, basilico, olio extravergine d\'oliva',
+      prezzo: 6,
+      allergeni: []
+    }
+  ]
+
   return (
-    <div>ViewMenu</div>
+    <div>
+      {menu.map(item => (
+        <MenuItem
+          key={item.nome}
+          nome={item.nome}
+          descrizione={item.descrizione}
+          prezzo={item.prezzo}
+          allergeni={item.allergeni}
+        />
+      ))}
+    </div>
   )
 }
 
