86f9d3a28311e1392e4a11583ca92dadb8943095
Marco-Burattoni <marco.burattoni@studio.unibo.it>
2023-02-02 10:23:26 +0100

    viewcontrolloordini

--- uwait/src/view/InterfacciaPersonale/ViewControlloOrdini.jsx
+++ uwait/src/view/InterfacciaPersonale/ViewControlloOrdini.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import OrdineItem from '../components/OrdineItem'
 
 function ViewControlloOrdini({ordini}) {
   return (
-    <div>ViewControlloOrdini</div>
+    <div>ViewControlloOrdini
+      {ordini.map((item) => (
+        <OrdineItem
+          portate={item.portate}
+          totale={item.totale}
+          tavolo={item.tavolo}
+          data={item.data}
+          stato={item.stato}
+        />
+      ))}
+    </div>
   )
 }
 
