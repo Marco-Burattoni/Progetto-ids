fab50a0d8c3fc7afc28fa4ba1cfb0b1d24bc224b
luca-eidos <luca.giovannini@eidos-tech.com>
2023-02-06 12:47:12 +0100

    Update ViewControlloOrdini.jsx

--- uwait/src/view/InterfacciaPersonale/ViewControlloOrdini.jsx
+++ uwait/src/view/InterfacciaPersonale/ViewControlloOrdini.jsx
@@ -1,27 +1,28 @@
-import React from 'react'
-import OrdineItem from '../components/OrdineItem'
-import { GestionePersonaleController } from "../../controller/personale.controller"
-import Button from '@material-ui/core/Button';
+import React, { useContext } from "react";
+import OrdineItem from "../components/OrdineItem";
+import { GestionePersonaleController } from "../../controller/personale.controller";
+import Button from "@material-ui/core/Button";
 import { useNavigate } from "react-router-dom";
+import { AppContext } from "../../App";
 
 function ViewControlloOrdini({ ordini }) {
-  
-  const { ordine, setOrdine, tavolo, setTavolo, personale, attivita} = useContext(AppContext);
+  const { ordine, setOrdine, tavolo, setTavolo, personale, attivita } =
+    useContext(AppContext);
   const navigate = useNavigate();
 
   const handleConsegnaOrdine = (ordine) => {
     const controller = new GestionePersonaleController(personale, attivita);
     controller.setConsegnato(ordine);
-    
-  }
+  };
 
   //per servire un tavolo accedo al menu
   const handleServiTavolo = () => {
     navigate("../menu");
-  }
+  };
 
   return (
-    <div>ViewControlloOrdini
+    <div>
+      ViewControlloOrdini
       {ordini.map((item) => (
         <div>
           <OrdineItem
@@ -31,16 +32,24 @@ function ViewControlloOrdini({ ordini }) {
             data={item.data}
             stato={item.stato}
           />
-          <Button variant="contained" color="primary" onClick={() => handleConsegnaOrdine(item)}>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => handleConsegnaOrdine(item)}
+          >
             Consegna ordine
           </Button>
         </div>
       ))}
-      <Button variant="contained" color="primary" onClick={() => handleServiTavolo(item)}>
-            Servi un tavolo
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => handleServiTavolo(0)}
+      >
+        Servi un tavolo
       </Button>
     </div>
-  )
+  );
 }
 
-export default ViewControlloOrdini
+export default ViewControlloOrdini;
