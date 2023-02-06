2f8dd2dfd15412bbbd45b2c8ebcf93db3d45dd8d
Marco-Burattoni <marco.burattoni@studio.unibo.it>
2023-02-06 09:20:16 +0100

    viewControlloOrdini

--- uwait/src/view/InterfacciaPersonale/ViewControlloOrdini.jsx
+++ uwait/src/view/InterfacciaPersonale/ViewControlloOrdini.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
 import OrdineItem from '../components/OrdineItem'
+import { GestionePersonaleController } from "../../controller/personale.controller"
+import Button from '@material-ui/core/Button';
 
-function ViewControlloOrdini({ordini}) {
+function ViewControlloOrdini({ ordini }) {
+  
+  const handleConsegnaOrdine = (ordine) => {
+    const controller = new GestionePersonaleController(personale, attivita);
+    controller.setConsegnato(ordine);
+  }
+
   return (
     <div>ViewControlloOrdini
       {ordini.map((item) => (
-        <OrdineItem
-          portate={item.portate}
-          totale={item.totale}
-          tavolo={item.tavolo}
-          data={item.data}
-          stato={item.stato}
-        />
+        <div>
+          <OrdineItem
+            portate={item.portate}
+            totale={item.totale}
+            tavolo={item.tavolo}
+            data={item.data}
+            stato={item.stato}
+          />
+          <Button variant="contained" color="primary" onClick={() => handleConsegnaOrdine(item)}>
+            Consegna ordine
+          </Button>
+        </div>
       ))}
     </div>
   )
